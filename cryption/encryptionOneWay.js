d9f3a4c799e4ad839700b864a6f013a790686abe
zagood <ljbfif50@gmail.com>
2020-12-23 16:44:14 +0900

    암호화 복호화 test : 현재 복호화 출력 안됨

--- cryption/encryptionOneWay.js
+++ cryption/encryptionOneWay.js
@@ -6,16 +6,14 @@ const CryptoJS = require('crypto-js');
 
 /** 테스트 input */
 const TEST_PASSWORD = 'password';
-const ADMIN_LOGIN = 'adminCertification';
-const USER_LOGIN = 'userCertification';
 
 /**
  * 관리자 계정 암호화
  */
-const encrypt = function(dataPw, division) {
+const encrypt = function (dataPw) {
     //관리자인증에서 salt를 붙여서 사용자 비밀번호 암호화
-    const shaSalt = "mcnc";
-    const shaPw = CryptoJS.SHA256(dataPw + shaSalt).toString();
+    const sha256Salt = "mcnc";
+    const sha256Password = CryptoJS.SHA256(dataPw + sha256Salt).toString();
     //사용자 인증에서 사용자 비밀번호
     const password = dataPw;
     //이니셜 벡터 (암호화 알고리즘에서 필요)
@@ -26,33 +24,23 @@ const encrypt = function(dataPw, division) {
     const securityKey = "A조김예은";
 
     //PBKDF2 키 생성
-    const key256Bits100Iterations =
+    const key256Bits10000Iterations =
         CryptoJS.PBKDF2(securityKey, CryptoJS.enc.Hex.parse(salt), {
             keySize: keySize / 32,
             iterations: iterationCount
         });
     //관리자인증
-    let encrypted;
-    if (division == "adminCertification") {
-        encrypted = CryptoJS.AES.encrypt(
-            shaPw,
-            key256Bits100Iterations, {
-                iv: CryptoJS.enc.Hex.parse(iv)
-            });
-    }
-    //사용자 인증
-    else if (division == "userCertification") {
-        encrypted = CryptoJS.AES.encrypt(
-            password,
-            key256Bits100Iterations, {
-                iv: CryptoJS.enc.Hex.parse(iv)
-            });
-    }
+    let encrypted = CryptoJS.AES.encrypt(
+        sha256Password,
+        key256Bits10000Iterations, {
+            iv: CryptoJS.enc.Hex.parse(iv)
+        });
+
     const encryptedString = encrypted.toString();
     const passwd = iv + encryptedString + salt;
     return passwd;
 }
 
-const returnPassword = encrypt(TEST_PASSWORD, ADMIN_LOGIN);
+const returnPassword = encrypt(TEST_PASSWORD);
 
 console.log(returnPassword);
---
+++ cryption/encryptionTwoWay.js
@@ -0,0 +1,44 @@
+const CryptoJS = require('crypto-js');
+
+// mainLogin(userId, encrypt($('.inpPw').val(), "adminCertification"))
+
+// const passwd = encrypt($("#pw").val(),"userCertification");
+
+/** 테스트 input */
+const TEST_PASSWORD = 'password';
+
+/**
+ * 관리자 계정 암호화
+ */
+const encrypt = function (dataPw) {
+    //관리자인증에서 salt를 붙여서 사용자 비밀번호 암호화
+    const sha256Salt = "mcnc";
+    const sha256Password = CryptoJS.SHA256(dataPw + sha256Salt).toString();
+    //사용자 인증에서 사용자 비밀번호
+    const password = dataPw;
+    //이니셜 벡터 (암호화 알고리즘에서 필요)
+    const iv = CryptoJS.lib.WordArray.random(128 / 8).toString(CryptoJS.enc.Hex);
+    const salt = CryptoJS.lib.WordArray.random(128 / 8).toString(CryptoJS.enc.Hex);
+    const keySize = 256;
+    const iterationCount = 10000;
+    const securityKey = "A조김예은";
+
+    //PBKDF2 키 생성
+    const key256Bits10000Iterations =
+        CryptoJS.PBKDF2(securityKey, CryptoJS.enc.Hex.parse(salt), {
+            keySize: keySize / 32,
+            iterations: iterationCount
+        });
+    let encrypted = CryptoJS.AES.encrypt(
+        password,
+        key256Bits10000Iterations, {
+            iv: CryptoJS.enc.Hex.parse(iv)
+        });
+    const encryptedString = encrypted.toString();
+    const passwd = iv + encryptedString + salt;
+    return passwd;
+}
+
+const returnPassword = encrypt(TEST_PASSWORD);
+
+console.log(returnPassword);
