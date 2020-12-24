11783c3f5e595a2a1e1150803c5c44378c153ec0
zagood <ljbfif50@gmail.com>
2020-12-24 23:14:32 +0900

    복호화 코드 정리

--- clientConnect/decryption.js
+++ clientConnect/decryption.js
@@ -6,7 +6,8 @@ const testPassword = '516ac37d279a0fdbea73b11e0d313c2cHVB1QWx29jBmrEtiAELDsw==52
 
 const CryptoJS = require('crypto-js');
 
-const pbkdf2AesDecrypt = function (salt, iv, SecurityKey, encryptedPasswd, iterationCount, keySize) {
+const pbkdf2AesDecrypt = function (salt, iv, SecurityKey, encryptedPasswd, 
+    iterationCount, keySize) {
     // AES -> PBKDF2 로 변경
 
     const key256Bits10000Iterations =
@@ -27,7 +28,8 @@ const pbkdf2AesDecrypt = function (salt, iv, SecurityKey, encryptedPasswd, itera
 
 }
 
-const sortingEncryptedWord = function (saltSize, ivSize, encryptedWord, SecurityKey, iterationCount, keySize) {
+const sortingEncryptedWord = function (saltSize, ivSize, encryptedWord, 
+    SecurityKey, iterationCount, keySize) {
 
     /** 암호문의 전체 길이*/
     const cryptPasswdLength = encryptedWord.length;
@@ -38,7 +40,8 @@ const sortingEncryptedWord = function (saltSize, ivSize, encryptedWord, Security
     /** 암호문에서 salt를 saltSize로 잘라줌 */
     const salt = encryptedWord.substring(cryptPasswdLength - saltSize, cryptPasswdLength);
 
-    return pbkdf2AesDecrypt(salt, iv, SecurityKey, encryptedPasswd, iterationCount, keySize);
+    return pbkdf2AesDecrypt(salt, iv, SecurityKey, encryptedPasswd, 
+        iterationCount, keySize);
 }
 
 console.log(sortingEncryptedWord(32, 32, testPassword, 'A조김예은', 10000, 256));
